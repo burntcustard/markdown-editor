b8e88aeebf7c5dc1299030927dede3671d6418fd
John Evans <john@wearelighthouse.com>
2024-11-11 04:04:46 +0000

    Trying a different probably temp app name & removing of logo in top left

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import appLogo from '/favicon.svg'
 import PWABadge from './PWABadge.tsx'
 
 import './App.css'
@@ -7,12 +6,7 @@ import { Tabs } from './Tabs.tsx'
 function App() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={appLogo} class="logo" alt="random-encounter-pwa logo" />
-        </a>
-      </div>
-      <h1>random-encounter-pwa</h1>
+      <h1 class="app-title">Markdown Editor PWA</h1>
       <Tabs/>
       <PWABadge />
     </>
