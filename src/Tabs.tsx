830a17d51ca02fbf31e0b75776562d2ea1f0ce8a
burntcustard <burntcustard@gmail.com>
2024-11-13 11:58:57 +0000

    Remove unnecessary console.log()s

--- src/Tabs.tsx
+++ src/Tabs.tsx
@@ -39,8 +39,6 @@ export const Tabs = () => {
   }
 
   const updateTab = (id: number, rawText: string) => {
-    console.log('tab content updated?')
-    console.log('id', id);
     setTabStore(
       tabData => tabData.id === id,
       produce((tabData) => {
