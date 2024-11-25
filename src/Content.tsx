3e3a86835c92047b3212db7a538290da52722d92
John Evans <john@wearelighthouse.com>
2024-11-25 14:08:51 +0000

    Add remarkGfm

--- src/Content.tsx
+++ src/Content.tsx
@@ -1,6 +1,7 @@
 import { Component } from 'solid-js'
 import { SolidMarkdown } from 'solid-markdown'
 import rehypeRaw from 'rehype-raw'
+import remarkGfm from 'remark-gfm'
 import './Content.css'
 
 const Content: Component<{children?: string}> = ({ children }) => (
@@ -8,6 +9,7 @@ const Content: Component<{children?: string}> = ({ children }) => (
     class="content"
     children={children}
     rehypePlugins={[rehypeRaw] as any}
+    remarkPlugins={[remarkGfm] as any}
   />
 )
 
